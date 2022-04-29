7ae83bee5bf0026db678d9f0930d5906832f156e
魏瑞峰 <weiruifeng@maoyan.com>
2022-04-29 18:59:00 +0800

    ci:搭建框架

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,16 +1,4 @@
 import 'egg';
-import { StoreClient } from '@dp/squirrel-client';
-import { User } from 'interface';
-import { ILabelOption } from 'interface';
 import { EggLogger } from 'egg';
 declare module 'egg' {
-  interface Context {
-    setContacts(this: Context): Promise<void>,
-    visitor: User,
-    saleSystems?: ILabelOption[],
-  }
-
-  interface Application {
-    redisStoreClient: StoreClient,
-  }
 }
